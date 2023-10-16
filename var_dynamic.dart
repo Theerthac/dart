2755db8d13b6a294681eb71765845c2bf7994f2a
Theerthac <127188105+Theerthac@users.noreply.github.com>
2023-10-16 12:28:51 +0530

    var and dynamic

---
+++ var_dynamic.dart
@@ -0,0 +1,14 @@
+// void main(List<String> args) {
+//   var a = "abc";
+//   //a = 10;
+//   a = "hhhh";
+//   print(a);
+// }
+
+void main(List<String> args) {
+  dynamic b = "theertha";
+
+  b = 60;
+  b = "hi";
+  print(b);
+}
